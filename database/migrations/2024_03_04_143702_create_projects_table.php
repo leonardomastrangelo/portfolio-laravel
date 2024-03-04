a784b42b6473c00ee62e8cf7527f3af2abe61761
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-03-04 16:01:46 +0100

    migrations

--- database/migrations/2024_03_04_143702_create_projects_table.php
+++ database/migrations/2024_03_04_143702_create_projects_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('slug', 70);
+            $table->text('description');
+            $table->boolean('status');
+            $table->string('preview', 255);
+            $table->string('logo', 255);
+            $table->string('type', 50);
+            $table->string('github', 255);
+            $table->string('link', 255);
+            $table->boolean('team');
             $table->timestamps();
         });
     }
